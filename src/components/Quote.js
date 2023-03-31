b6c9d1f4f4853b0cd45f6fc64e44b5a9085edad5
Muhammad <umedjon02.ud@gmail.com>
2023-03-31 17:55:22 +0500

    Add quotes to the webpage

---
+++ src/components/Quote.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+
+export default function Quote() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const result = await fetch('https://api.api-ninjas.com/v1/quotes', {
+          method: 'GET',
+          headers: {
+            'X-Api-Key': '6d3biugl0XQ3WcBvtFawHw==eTmgR9F1cXsHz5wO',
+            'Content-Type': 'application/json',
+          },
+        });
+        const json = await result.json();
+        setData(json);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [setData]);
+
+  if (hasError) return <div>Something went wrong!</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <>
+      <div>
+        {data.map((item) => (
+          <p key={item.author}>{item.quote}</p>
+        ))}
+      </div>
+    </>
+
+  );
+}
