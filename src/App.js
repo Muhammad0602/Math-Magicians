e60f943a65277c6a683a1c1207d124e5b578a9e7
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-04-05 18:28:20 +0500

    Add all the pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Header from './components/Header';
+import {Route, Routes}from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <Quote />
-    </div>
+    <>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes> 
+    </>
+      
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Welcome to our Math Magicians website. Where you can do simple math calculations as well as reading education related quotes.</div>
+  )
+}
+
+export default Home
